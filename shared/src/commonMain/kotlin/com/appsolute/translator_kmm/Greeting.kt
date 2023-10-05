570ba910d349fa1234f7dfd468485d87b83c3056
Toan (Alex) Duong <braveheart3208@gmail.com>
2023-10-05 21:45:07 +1100

    Initial Commit

---
+++ shared/src/commonMain/kotlin/com/appsolute/translator_kmm/Greeting.kt
@@ -0,0 +1,9 @@
+package com.appsolute.translator_kmm
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
